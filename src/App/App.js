831e89b6847dbfef7688683ff9a6e44a180975b2
Joe Chirchirillo <cchirchirillo3@gmail.com>
2024-11-12 19:17:38 -0700

    feat: render info from api

--- src/App/App.js
+++ src/App/App.js
@@ -1,18 +1,16 @@
-import logo from '../logo.svg';
+// import logo from '../logo.svg';
 import './App.css';
-import LandingPage from "./LandingPage"
-import SubscriptionDetailsPage from "./SubscriptionDetailsPage"
-import { Routes, Route, Link, useNavigate, useParams } from "react-router-dom";
+import LandingPage from "../LandingPage/LandingPage"
+import SubscriptionDetailsPage from "../SubscriptionDetailsPage/SubscriptionDetailsPage"
 
+import { Routes, Route, Link, useNavigate, useParams } from "react-router-dom";
 
 function App() {
   return (
-  //  <Router>
     <Routes>
       <Route path="/" element={<LandingPage/>}/>
       <Route path="/subscriptions/:id" element={<SubscriptionDetailsPage />} />
     </Routes>
-  //  </Router>
   );
 }
 
--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -1,2 +1,61 @@
-import styles from './LandingPage.css'
+import './LandingPage.css';
+import { useState, useEffect } from 'react';
+import {Link} from 'react-router-dom';
+import logo from '../pix/TDD_logo.png';
 
+
+
+function LandingPage() {
+  const [subscriptions, setSubscriptions] = useState([]);
+
+  function getSubscriptions() {
+    fetch(
+      "http://127.0.0.1:3000/api/v1/subscriptions"
+    )
+    .then((response) => {
+      console.log("RESPONSE HERE:", response)
+      if (!response.ok){
+        throw new Error("Failed to fetch Subscriptions");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log("WOW WE GOT DATA", data)
+      console.log("WOW WE GOT MOOORE DATA", data.data)
+      setSubscriptions(data.data);
+    })
+    .catch((error) => {
+      console.error("Error fetching subscriptions:", error)
+    })
+  }
+console.log("LOOK HERE",subscriptions)
+  useEffect(() => {
+    getSubscriptions();
+  }, []);
+
+  return (
+   
+    <div className='sub-container'>
+      <header>
+        <h1>Tea Digital Database</h1>
+        <img src={logo} alt="logo of website"></img>
+      </header>
+      
+      <div className='sub-body'>
+        <ul className='sub-list'>
+          {subscriptions.length > 0 ? (
+            subscriptions.map((sub) => (
+              <li key={sub.id}>
+                <Link to={`/subscriptions/${sub.id}`}>{sub.attributes.title}</Link>
+              </li>
+            ))
+          ) : (
+            <p>Make some tea while I'm loading all subscriptions</p>
+          )
+        }
+        </ul>
+      </div>
+    </div>
+  );
+}
+export default LandingPage;
